13eaf4c1b2c175b9ccfab4f16e1cafeb2db28144
Josh Cardif <josh.cardif14@outlook.com>
2018-09-08 12:20:10 +0300

    Implement Interfac

--- XamarinFirebasePhoneAuth/MainActivity.cs
+++ XamarinFirebasePhoneAuth/MainActivity.cs
@@ -3,17 +3,23 @@ using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
 using Android.Support.Design.Widget;
+using Android.Views;
 using Android.Widget;
 using Com.Mukesh.CountryPickerLib;
+using Firebase;
+using Firebase.Auth;
+using Java.Util.Concurrent;
 
 namespace XamarinFirebasePhoneAuth
 {
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
-    public class MainActivity : AppCompatActivity, IOnCountryPickerListener
+    public class MainActivity : AppCompatActivity, IOnCountryPickerListener, IUpdatable
     {
         private TextInputEditText _codeInputEditText;
         private TextInputEditText _phoneNoInputEditText;
         private Button _signInButton;
+        public static FirebaseAuth _auth;
+        public static FirebaseApp _app;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
@@ -50,12 +56,40 @@ namespace XamarinFirebasePhoneAuth
 
         private void SignInUser(string phoneNo)
         {
-                Toast.MakeText(this, "Lets do  it", ToastLength.Short).Show();
+            InitFirebaseAuth();
+            var callbacks=new PhoneAuthCallbacks(this);
+            PhoneAuthProvider.GetInstance(_auth)
+                .VerifyPhoneNumber(
+                    phoneNo,
+                    2,
+                    TimeUnit.Minutes,
+                    this,
+                    callbacks);
+        }
+
+        private void InitFirebaseAuth()
+        {
+            if (_app == null)
+                _app = FirebaseApp.InitializeApp(this);
+            _auth=new FirebaseAuth(_app);
+        }
+
+        public void UpdateUi()
+        {
+            _codeInputEditText.Visibility = ViewStates.Invisible;
+            _phoneNoInputEditText.Visibility = ViewStates.Invisible;
+            _signInButton.Text = "Continue";
         }
 
         public void OnSelectCountry(Country country)
         {
             _codeInputEditText.Text = country.DialCode;
+        }
+
+        public void IsSuccessful(bool isSuccessful)
+        {
+            if(isSuccessful)
+                UpdateUi();
         }
     }
 }
--- XamarinFirebasePhoneAuth/PhoneAuthCallbacks.cs
+++ XamarinFirebasePhoneAuth/PhoneAuthCallbacks.cs
@@ -5,25 +5,55 @@ using System.Text;
 
 using Android.App;
 using Android.Content;
+using Android.Gms.Tasks;
 using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Firebase;
 using Firebase.Auth;
+using static Firebase.Auth.PhoneAuthProvider;
 
 namespace XamarinFirebasePhoneAuth
 {
-    public class PhoneAuthCallbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
+    public class PhoneAuthCallbacks : OnVerificationStateChangedCallbacks, IOnCompleteListener
     {
+        private string _verificationId;
+        private ForceResendingToken _token;
+        private IUpdatable updatable;
+
+        public PhoneAuthCallbacks(IUpdatable updatable)
+        {
+            this.updatable = updatable;
+        }
+
         public override void OnVerificationCompleted(PhoneAuthCredential credential)
         {
-            throw new NotImplementedException();
+            SignInWithPhoneAuthCredential(credential);
+        }
+
+        private void SignInWithPhoneAuthCredential(PhoneAuthCredential credential)
+        {
+            MainActivity._auth.SignInWithCredential(credential)
+                .AddOnCompleteListener(this);
+
         }
 
         public override void OnVerificationFailed(FirebaseException exception)
         {
             throw new NotImplementedException();
+        }
+
+        public override void OnCodeSent(string verificationId, ForceResendingToken forceResendingToken)
+        {
+            base.OnCodeSent(verificationId, forceResendingToken);
+            _verificationId = verificationId;
+            _token = forceResendingToken;
+        }
+
+        public void OnComplete(Task task)
+        {
+          updatable.IsSuccessful(true);
         }
     }
 }
