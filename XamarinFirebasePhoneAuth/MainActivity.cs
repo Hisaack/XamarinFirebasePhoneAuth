cf18b217b483c4f71e0af95a1ef3a9115fc39bb0
Josh Cardif <josh.cardif14@outlook.com>
2018-09-08 09:56:27 +0300

    WireUp the layout

--- XamarinFirebasePhoneAuth/MainActivity.cs
+++ XamarinFirebasePhoneAuth/MainActivity.cs
@@ -2,6 +2,7 @@
 using Android.OS;
 using Android.Support.V7.App;
 using Android.Runtime;
+using Android.Support.Design.Widget;
 using Android.Widget;
 
 namespace XamarinFirebasePhoneAuth
@@ -9,11 +10,17 @@ namespace XamarinFirebasePhoneAuth
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
     public class MainActivity : AppCompatActivity
     {
+        private TextInputEditText _codeInputEditText;
+        private TextInputEditText _phoneNoInputEditText;
+        private Button _signInButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
+            _codeInputEditText = FindViewById<TextInputEditText>(Resource.Id.countryCodeTxtInputEdtTxt);
+            _phoneNoInputEditText =FindViewById<TextInputEditText>(Resource.Id.phoneNumberTxtInputEdtTxt);
+            _signInButton = FindViewById<Button>(Resource.Id.authorizeVerificationBtn);
         }
     }
 }
