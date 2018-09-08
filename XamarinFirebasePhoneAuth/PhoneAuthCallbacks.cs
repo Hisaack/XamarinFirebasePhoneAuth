ef5552e6f70fd42dbebb27445c949b3ea988779c
Josh Cardif <josh.cardif14@outlook.com>
2018-09-08 10:25:22 +0300

    edit gitIgnore

--- XamarinFirebasePhoneAuth/PhoneAuthCallbacks.cs
+++ XamarinFirebasePhoneAuth/PhoneAuthCallbacks.cs
@@ -9,11 +9,21 @@ using Android.OS;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
+using Firebase;
+using Firebase.Auth;
 
 namespace XamarinFirebasePhoneAuth
 {
-    public class PhoneAuthCallbacks 
+    public class PhoneAuthCallbacks : PhoneAuthProvider.OnVerificationStateChangedCallbacks
     {
+        public override void OnVerificationCompleted(PhoneAuthCredential credential)
+        {
+            throw new NotImplementedException();
+        }
 
+        public override void OnVerificationFailed(FirebaseException exception)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
